3be77810a9d4812e25ed73e1eee17465ff5ad255
Anne Apiyo <anneapiyo0@gmail.com>
2024-06-10 16:14:57 +0300

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,13 @@
+ //void main(List<String> args) {
+  //print("Hello world");
+//}
+
+
+void main() {
+  int result =add(3, 5);
+  print("sum of 3 and 5 is $result");
+
+}
+int add(int a,int b){
+  return a+b;
+}
